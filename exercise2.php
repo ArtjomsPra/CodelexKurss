3c4e1d900db1f985361c11b31bff95bb2574a2ba
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-03-21 13:30:21 +0200

    Delete exercise2.php

--- exercise2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Dump the same values that should display both data type & its value. (Note, usage of var_dump)
-
-$int = 10;
-$float = 10.10;
-$string = 'Hello World!';
-
-var_dump($int, $float, $string);
