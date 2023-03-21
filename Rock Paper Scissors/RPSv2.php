d616af67341e2d61df7043440c632f1e2e7f81e5
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-03-21 13:44:44 +0200

    Add files via upload

---
+++ Rock Paper Scissors/RPSv2.php
@@ -0,0 +1,44 @@
+<?php
+
+//Every outcome in an array. Player selection is a key.
+//The values are another array with the possible outcomes against computer selection.
+$outcomes = [
+    'rock' => ['scissors' => 'win', 'paper' => 'lose', 'rock' => 'tie'],
+    'paper' => ['rock' => 'win', 'scissors' => 'lose', 'paper' => 'tie'],
+    'scissors' => ['paper' => 'win', 'rock' => 'lose', 'scissors' => 'tie'],
+    'lizard' => ['paper' => 'win', 'rock' => 'win', 'scissors' => 'win'],
+    'spock' => ['paper' => 'win', 'rock' => 'win', 'scissors' => 'win']
+];
+
+$playAgain = 'Y';
+
+while ($playAgain == 'Y') {
+//Selecting random key for a computer selection from array outcomes
+    $computerSelection = array_rand($outcomes);
+    $playerSelection = '';
+//While player doesnt select rock, paper or scissors from an array, it will ask him to enter his selection again.
+    while(!in_array($playerSelection, ['rock', 'paper', 'scissors', 'lizard', 'spock'])){
+        $playerSelection = strtolower(readline('Please enter your selection, rock, paper or scissors:'));
+    }
+
+    echo "Computer selected: $computerSelection\n";
+    echo "You selected: $playerSelection\n";
+
+    if ($outcomes[$playerSelection][$computerSelection] === 'win') {
+        echo 'You win!';
+    } elseif ($outcomes[$playerSelection][$computerSelection] === 'lose') {
+        echo 'You lose!';
+    } else {
+        echo 'It\'s a tie!';
+    }
+
+    echo PHP_EOL;
+    $playAgain = strtoupper(readline('Do you want to play again? (Y/N)'));
+    while ($playAgain !== 'Y' && $playAgain !== 'N') {
+        $playAgain = strtoupper(readline('Invalid option! Please enter Y or N: '));
+    }
+}
+
+echo 'Thanks for playing!';
+
+
