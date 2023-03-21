3c082dc42cb72d1afb573ed82b6695e7d8c992e6
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-03-21 13:33:23 +0200

    Add files via upload

---
+++ 01-varibles/exercise1.php
@@ -0,0 +1,9 @@
+<?php
+
+//Create variable that prints out an integer 10, float 10.10, string "hello world"
+
+$int = 10;
+$float = 10.10;
+$string = 'Hello World!';
+
+echo $int, $float, $string;
---
+++ 01-varibles/exercise3.php
@@ -0,0 +1,11 @@
+<?php
+
+//Concatenate your name, surname and integer of your age.
+
+$name = 'Artjoms';
+$surname = 'Rencis-Pralics';
+$age = 32;
+
+echo $name . ' ' . $surname . ' ' . $age . PHP_EOL;
+
+echo "$name $surname $age";
