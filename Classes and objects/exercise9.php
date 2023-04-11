d399694083ee97f7daa00b09de7af604fcab2f6b
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-11 14:46:53 +0300

    Add files via upload

---
+++ Classes and objects/exercise9.php
@@ -0,0 +1,26 @@
+<?php
+
+class BankAccount
+{
+    private $name;
+    private $balance;
+
+    public function __construct($name, $balance)
+    {
+        $this->name = $name;
+        $this->balance = $balance;
+    }
+
+    function show_user_name_and_balance() {
+        $balance_formatted = number_format(abs($this->balance), 2, '.', '');
+        $balance_prefix = ($this->balance < 0) ? '-$' : '$';
+        return $this->name . ', ' . $balance_prefix . $balance_formatted . PHP_EOL;
+    }
+
+}
+
+$ben = new BankAccount('Benson', 17.25);
+echo $ben->show_user_name_and_balance();
+
+$jan = new BankAccount('Janis', -17.5);
+echo $jan->show_user_name_and_balance();
