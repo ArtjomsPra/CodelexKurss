c9170d34fc8e0e584ebfb9361ab09791c38164bc
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-14 12:11:08 +0300

    Update weatherReport.php

--- Homeworks/weatherReport.php
+++ Homeworks/weatherReport.php
@@ -1,19 +1,24 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
 $city = readline("Hello and welcome to the simplest weather report! Enter city: ");
-$apiKey = "";
+$apiKey = "3b93430fef65e8eddf6d676047d68e7f";
 $url = "http://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=" . $apiKey . "&units=metric";
-$data = file_get_contents($url);
+
+$client = new Client();
+$response = $client->get($url);
+$data = json_decode($response->getBody(), true);
 
 if ($data) {
-    $weatherData = json_decode($data, true);
-
     echo "Weather for " . $city . "\n";
-    echo "Temperature: " . round($weatherData["main"]["temp"]) . "°C\n";
-    echo "Weather: " . $weatherData["weather"][0]["main"] . "\n";
-    echo "Description: " . $weatherData["weather"][0]["description"] . "\n";
-    echo "Humidity: " . $weatherData["main"]["humidity"] . "%\n";
-    echo "Wind speed: " . $weatherData["wind"]["speed"] . " m/s\n";
+    echo "Temperature: " . round($data["main"]["temp"]) . "°C\n";
+    echo "Weather: " . $data["weather"][0]["main"] . "\n";
+    echo "Description: " . $data["weather"][0]["description"] . "\n";
+    echo "Humidity: " . $data["main"]["humidity"] . "%\n";
+    echo "Wind speed: " . $data["wind"]["speed"] . " m/s\n";
 } else {
     echo "Error: Could not retrieve weather data for " . $city . "\n";
 }
