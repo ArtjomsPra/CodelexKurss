6403256efe2beae26b2eac3a515fc85264b4c8d6
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-14 18:23:57 +0300

    Update exercise11.php

--- Classes and objects/Exercise11/exercise11.php
+++ Classes and objects/Exercise11/exercise11.php
@@ -12,8 +12,8 @@ $myAccount = new Account('My account', 0);
 $mattsAccount->withdraw(100);
 $myAccount->deposit(100);
 
-$mattsAccount->showAccount();
-$myAccount->showAccount();
+echo $mattsAccount->showAccount();
+echo $myAccount->showAccount();
 
 $account_a = new Account("A", 100.0);
 $account_b = new Account("B", 0.0);
@@ -22,6 +22,6 @@ $account_c = new Account("C", 0.0);
 $account_a->transfer($account_a, $account_b, 50.0);
 $account_b->transfer($account_b, $account_c, 25.0);
 
-$account_a->showAccount();
-$account_b->showAccount();
-$account_c->showAccount();
+echo $account_a->showAccount();
+echo $account_b->showAccount();
+echo $account_c->showAccount();
